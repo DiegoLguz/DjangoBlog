7bfda582c3ac6d8fc61191009c1280187ff112e1
Diego Landaverde <diegolguz14@gmail.com>
2016-09-29 17:46:18 +0200

    Template modificado para mostrar los post de la base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Postear
 
 def post_list(request):
-        return render(request, 'blog/lista.html', {})
+    posts = Postear.objects.all()
+    return render(request, 'blog/lista.html', {'posts': posts})
