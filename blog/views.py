573c187b8f98f2ad486232e45197f558a31277d3
Diego Landaverde <diegolguz14@gmail.com>
2016-09-30 17:15:46 +0200

    Se creo la pagina de Error 404 not found para la pagina

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postear
 
 def post_list(request):
     posts = Postear.objects.all()
     return render(request, 'blog/lista.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
